c3f9b7a37e50330e3d9f213ca77626374a5ef3ca
MAŁGORZATA <s31606@pjwstk.edu.pl>
2024-04-24 13:45:07 +0200

    PGO7

---
+++ PGO7/src/Circle.java
@@ -0,0 +1,15 @@
+public class Circle implements Figure{
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getPerimeter() {
+        return 0;
+    }
+
+    public double getArea() {
+        return 0;
+    }
+}
---
+++ PGO7/src/Rectangle.java
@@ -0,0 +1,9 @@
+public class Rectangle implements Figure{
+    public double getPerimeter() {
+        return 0;
+    }
+
+    public double getArea() {
+        return 0;
+    }
+}
